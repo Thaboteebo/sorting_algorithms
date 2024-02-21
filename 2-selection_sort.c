9492e190a4b77e2177ad005b705091233a86f724
Thaboteebo <Thaboteebo01@gmail.com>
2024-02-21 00:55:11 -0800

    Selection sort

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -17,7 +17,7 @@ void swap_ints(int *a, int *b)
 
 /**
  * selection_sort - Sort an array of integers in ascending order
- * using the selection sort sort algorithm.
+ * using the selection sort algorithm.
  * @array: An array of integers.
  * @size: The size of the array.
  *
@@ -27,20 +27,20 @@ void swap_ints(int *a, int *b)
 void selection_sort(int *array, size_t size)
 {
 	int *min;
-	size_t i, j;
+	size_t j, k;
 
 	if (array == NULL || size < 2)
 		return;
 
-	for (i = 0; i < size - 1; i++)
+	for (j = 0; j < size - 1; j++)
 	{
-		min = array + i;
-		for (j = i + 1; j < size; j++)
-			min = (array[j] < *min) ? (array + j) : min;
+		min = array + j;
+		for (k = j + 1; k < size; k++)
+			min = (array[k] < *min) ? (array + k) : min;
 
-		if ((array + i) != min)
+		if ((array + j) != min)
 		{
-			swap_ints(array + 1, min);
+			swap_ints(array + j, min);
 			print_array(array, size);
 		}
 	}
